2bc96b196e40e51563b07e55499c967e2cf14271
DiegoOTdC <studioartdiego@gmail.com>
2020-06-25 21:33:05 +0200

    Gave function an argument

--- cars.js
+++ cars.js
@@ -1,7 +1,9 @@
 const makers = ["ferrari", "BMW", "VW"]
 
-function car() {
-    return 'car'
+function car(maker) {
+    const mission = `${maker} makes cars`
+    
+    return mission
 }
 
 const messages = makers.map(car)
