790c88fdc3e246912d37005ce0ac9ca02150e461
DiegoOTdC <studioartdiego@gmail.com>
2020-06-25 21:55:38 +0200

    Registered endpoint and send string response

--- knowledge/index.js
+++ knowledge/index.js
@@ -2,6 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.get(
+    '/know',
+    (request, response) => {
+        response.send('I know')
+    }
+)
+
 const port = 3000
 
 app.listen(
