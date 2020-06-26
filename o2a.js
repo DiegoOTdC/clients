77930f5f8bb27a29a78e8df0e64b0ebd1dfa96d1
DiegoOTdC <studioartdiego@gmail.com>
2020-06-26 11:32:06 +0200

    log some keys - object to array

---
+++ o2a.js
@@ -0,0 +1,9 @@
+const object = {
+    database: "A database, is a library with stored data. Like Axios, which is a library for requesting data over the internet",
+    API: "And API is a server that does NOT send HTML. Instead, APIs sends only data, often in JSON" ,
+    client: "Client is a program that requests data from a server"
+}
+
+const output = Object.keys(object)
+
+console.log(output)
