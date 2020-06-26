97302a9f6b897670c607c728221d99b9c3d3546a
DiegoOTdC <studioartdiego@gmail.com>
2020-06-26 11:47:40 +0200

    map over entries

--- o2a.js
+++ o2a.js
@@ -1,13 +1,22 @@
 const object = {
     database: "A database, is a library with stored data. Like Axios, which is a library for requesting data over the internet",
-    API: "And API is a server that does NOT send HTML. Instead, APIs sends only data, often in JSON" ,
-    client: "Client is a program that requests data from a server"
+    API: "An API is a server that does NOT send HTML. Instead, APIs sends only data, often in JSON" ,
+    client: "A client is a program that requests data from a server"
 }
 
-const output = Object.keys(object)
+const keys = Object.keys(object)
+console.log("output keys:", keys)
 
-console.log(output)
+const values = Object.values(object)
+console.log("output values:", values)
 
-const output2 = Object.values(object)
+const entries = Object.entries(object)
+console.log("output entries:", entries)
 
-console.log(output2)
+const explanations = entries.map(entry => {
+    const [subject, explanation] = entry
+
+    return `An explanation of ${subject}: ${explanation}`
+})
+
+console.log("output explanations:", explanations)
