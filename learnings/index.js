0b40848cbde6f8413c70599a024210b93a6747bb
DiegoOTdC <studioartdiego@gmail.com>
2020-06-26 10:30:25 +0200

    send a Json

--- learnings/index.js
+++ learnings/index.js
@@ -2,6 +2,19 @@ const express = require("express")
 
 const app = express()
 
+app.get(
+    "/json",
+    (request, response) => {
+        const days = {
+            yesterday: "I learned about servers!",
+            today: "I learned about clients!"
+        }
+
+        response.send(days)
+    }
+
+)
+
 const port = 3000
 
 app.listen(
